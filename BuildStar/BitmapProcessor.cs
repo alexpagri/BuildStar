ddba3552d47a1bc334d768b6d4fd64ca76a0aa76
Gerge <cpupett@yahoo.com>
2019-10-17 21:58:34 +0300

    Added image loading as bitmaps

---
+++ BuildStar/BitmapProcessor.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuildStar
+{
+    class BitmapProcessor
+    {
+
+        public static List<BlockModel> LoadImages(string path)
+        {
+            List<BlockModel> result = new List<BlockModel>();
+            DirectoryInfo d = new DirectoryInfo(path);
+            FileInfo[] Files = d.GetFiles("*.*");
+            foreach (var file in Files)
+            {
+                try
+                {
+                    BlockModel auxBlock = new BlockModel();
+                    auxBlock.Name = Path.GetFileNameWithoutExtension(file.FullName);
+                    auxBlock.Image = (Bitmap)Image.FromFile(file.FullName);
+                    result.Add(auxBlock);
+                }
+                catch (Exception e)
+                {
+
+                }
+            }
+            return result;
+        }
+    }
+}
